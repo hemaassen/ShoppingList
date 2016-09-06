e07f9a74827a4c85779d04cbc66dd489074b9ed8
Administrator <f.neumann.berlin@googlemail.com>
2016-09-06 12:38:41 +0200

    first

--- app/src/main/java/de/example/frank/shoppinglist/ShoppingMemoDataSource.java
+++ app/src/main/java/de/example/frank/shoppinglist/ShoppingMemoDataSource.java
@@ -1,6 +1,8 @@
 package de.example.frank.shoppinglist;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
@@ -11,6 +13,13 @@ public class ShoppingMemoDataSource {
     private static final String LOG_TAG = ShoppingMemoDataSource.class.getSimpleName();
     private SQLiteDatabase db;
     private ShoppingMemoDbHelper helper;
+
+    private String[] columns= {
+            ShoppingMemoDbHelper.COLUMN_ID,
+            ShoppingMemoDbHelper.COLUMN_PRODUCT,
+            ShoppingMemoDbHelper.COLUMN_QUANTITY
+    };
+
 
     public ShoppingMemoDataSource(Context con){
         Log.d(LOG_TAG, " Der Helper wird erzeugt.");
@@ -27,4 +36,37 @@ public class ShoppingMemoDataSource {
         helper.close();
         Log.d(LOG_TAG,"Datenbank wieder geschlossen");
     }
+
+    public ShoppingMemo createShoppingMemo(String product, int quantity){
+        ContentValues values = new ContentValues();
+        values.put(ShoppingMemoDbHelper.COLUMN_PRODUCT,product);
+        values.put(ShoppingMemoDbHelper.COLUMN_QUANTITY, quantity);
+
+        long insertId = db.insert(ShoppingMemoDbHelper.TABLE_SHOPPING_LIST, null, values);
+
+        Cursor cursor = db.query(ShoppingMemoDbHelper.TABLE_SHOPPING_LIST,
+                columns, ShoppingMemoDbHelper.COLUMN_ID + "=" + insertId,
+                null, null, null, null);
+        cursor.moveToFirst();
+        ShoppingMemo shoppingMemo = cursorToShoppingMemo(cursor);
+        cursor.close();
+        return shoppingMemo;
+    }
+    private ShoppingMemo cursorToShoppingMemo(Cursor cursor) {
+
+        int idIndex = cursor.getColumnIndex(ShoppingMemoDbHelper.COLUMN_ID);
+        int idProduct = cursor.getColumnIndex(ShoppingMemoDbHelper.COLUMN_PRODUCT);
+        int idQuantity = cursor.getColumnIndex(ShoppingMemoDbHelper.COLUMN_QUANTITY);
+
+        String product = cursor.getString(idProduct);
+        int quantity = cursor.getInt(idQuantity);
+        long id = cursor.getLong(idIndex);
+
+        ShoppingMemo shoppingMemo = new ShoppingMemo(product, quantity, id);
+        return shoppingMemo;
+    }
+
+
+
+
 }
